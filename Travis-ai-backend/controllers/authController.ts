91fd18e0db15eebea1a8c73b18361b7b6543ca01
Ice Chain <afolabiyinka738@gmail.com>
2025-12-28 17:29:44 +0100

    Adding the frontend url to the CORs list

--- Travis-ai-backend/controllers/authController.ts
+++ Travis-ai-backend/controllers/authController.ts
@@ -16,7 +16,7 @@ export const loginUser = async (req: Request, res: Response) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "User not found" });
+      return res.status(404).json({ message: "User not found" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
